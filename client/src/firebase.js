7fdc77635f738009201abda59923ccc57812f473
yiwen6826 <luyiwen2006@gmail.com>
2026-04-09 15:44:07 -0400

    firebase storage

---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9fAPorijSyaPrUg4qa3iifIedqtO0SlM",
+  authDomain: "karaoke-machine-3a3a4.firebaseapp.com",
+  projectId: "karaoke-machine-3a3a4",
+  storageBucket: "karaoke-machine-3a3a4.firebasestorage.app",
+  messagingSenderId: "627172251106",
+  appId: "1:627172251106:web:798ae6c594e620ac5f15bb",
+  measurementId: "G-7SF3L5KHY6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
