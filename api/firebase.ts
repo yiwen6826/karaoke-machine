42c8d883cb0017ce666ce958f749820286a3946d
yiwen6826 <luyiwen2006@gmail.com>
2026-05-03 10:57:33 -0400

    update service account

--- api/firebase.ts
+++ api/firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase-admin/firestore';
 import path from 'path';
 
 const serviceAccountPath = path.resolve(__dirname, './service_account.json');
-const serviceAccount = require(serviceAccountPath);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 const app = initializeApp({
   credential: cert(serviceAccount),
   projectId: "karaoke-machine-3a3a4",
