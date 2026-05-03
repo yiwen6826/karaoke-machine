7e092d10570d81eb5a43d6462672fd66661cfc06
yiwen6826 <luyiwen2006@gmail.com>
2026-05-03 10:05:08 -0400

    fix server type errors

--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,10 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { SONG_LIBRARY } from "./constants/song-library";
 import { db } from './firebase';
 import { FieldValue } from 'firebase-admin/firestore';
 
-const app: Express = express();
+const app = express() as any;
 const port = 8080;
 
 app.use(cors());
@@ -29,15 +29,15 @@ app.use(cors({
 // GET
 // send sorted queue of each user (min priority queue i.e. lowest priority first)
 // needed for updating gui
-app.get("/api/queue/:uid", async (req, res) => {
+app.get("/api/queue/:uid", async (req: Request, res: Response) => {
   const {uid} = req.params;
   const snapshot = await db.collection("queue").where("userId", "==", uid).orderBy("priority", "asc").get();
-  const queue: queueEntry[] = snapshot.docs.map(doc => doc.data() as queueEntry);
+  const queue: queueEntry[] = snapshot.docs.map((doc: any) => doc.data() as queueEntry);
   res.json(queue);
 })
 
 // search queue
-app.get("/api/search", async (req, res) => {
+app.get("/api/search", async (req: Request, res: Response) => {
   const query = (req.query.q as string)?.toLowerCase();
 
   if (!query) {res.json([]); return;} // no query
@@ -50,7 +50,7 @@ app.get("/api/search", async (req, res) => {
 // add song to queue
 // If successful, returns status 201 and json of qEntry
 // Otherwise, returns status 404
-app.post("/api/queue", async (req, res) => {
+app.post("/api/queue", async (req: Request, res: Response) => {
   const {songId, uid} = req.body;
   const song = SONG_LIBRARY.find(s => s.id === songId);
   if (!uid) return res.status(400).send("No user provided");
@@ -74,7 +74,7 @@ app.post("/api/queue", async (req, res) => {
 // decrement song priority in queue (i.e. move it upward)
 // If successful, returns status 201.
 // Otherwise, returns status 404 if song not found; returns status 405 if song is first in queue and cannot change priority.
-app.put("/api/queue/:id/:uid", async (req, res) => {
+app.put("/api/queue/:id/:uid", async (req: Request, res: Response) => {
   const qidToBoost = parseInt(req.params.id);
   const {uid} = req.params;
 
@@ -111,7 +111,7 @@ app.put("/api/queue/:id/:uid", async (req, res) => {
 // DELETE
 // remove song from queue
 // Returns status 204
-app.delete("/api/queue/:id/:uid", async (req, res) => {
+app.delete("/api/queue/:id/:uid", async (req: Request, res: Response) => {
   const {id, uid} = req.params;
 
   if (!uid || uid === "undefined") {
