15826eca3daf351c62c36249eb6afd44db4b6a55
yiwen6826 <luyiwen2006@gmail.com>
2026-05-03 14:31:25 -0400

    vercel debug

--- api/firebase.ts
+++ api/firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase-admin/firestore';
 import path from 'path';
 
 const serviceAccountPath = path.resolve(__dirname, './service_account.json');
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+const serviceAccount = (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string) : require(serviceAccountPath);
 const app = initializeApp({
   credential: cert(serviceAccount),
   projectId: "karaoke-machine-3a3a4",
